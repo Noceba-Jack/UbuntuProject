2d237229a23e1781492737740d660efa49297af5
Nonceba Jack <155999468+Noceba-Jack@users.noreply.github.com>
2026-05-24 13:05:51 +0200

    Delete Ubuntu_MVC_Phone/Ubuntu directory
    delete the old files and add the new project files with the updated backend

--- Ubuntu_MVC_Phone/Ubuntu/Ubuntu/Models/ViewModels/CustomersViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ubuntu.Models.ViewModels
-{
-    public class CustomersViewModel
-    {
-        public string SearchText { get; set; } = string.Empty;
-        public string ActiveFilter { get; set; } = "All";
-        public List<Customer> FilteredCustomers { get; set; } = new();
-    }
-}
